323252caf6784f0e6bd3df7b24fe51beaa74f0ed
RitamMondal2468 <ritammondal700144@gmail.com>
2025-10-21 22:12:39 +0530

    Add Bubble Sort implementations for ascending and descending order

---
+++ 8_Sorting/Bubble_Sort_Ascending.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void inputArray(int arr[], int &n)
+{
+  cout << "Enter the array size: ";
+  cin >> n;
+  if (n < 1 || n > 1000)
+  {
+    cout << "Invalid array size!" << endl;
+    exit(1);
+  }
+  cout << "Enter array elements: ";
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+}
+
+void bubbleSort(int arr[], int n)
+{
+  for (int i = n - 2; i >= 0; i--)
+  {
+    bool swapped = false;
+
+    for (int j = 0; j <= i; j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
+        swap(arr[j], arr[j + 1]);
+        swapped = true;
+      }
+    }
+    if (!swapped)
+    {
+      break;
+    }
+  }
+}
+
+void printArray(int arr[], int n)
+{
+  cout << "After Bubble Sort: ";
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main()
+{
+  int arr[1000], n;
+
+  inputArray(arr, n);
+  bubbleSort(arr, n);
+  printArray(arr, n);
+}
